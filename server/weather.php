9df46564ae00e64edb72cd1c0ab5b65739a427e1
orviwan <orviwan@gmail.com>
2013-07-27 14:27:32 +0100

    Update weather.php
    Fixed icons, tidied slightly

--- server/weather.php
+++ server/weather.php
@@ -3,38 +3,43 @@ define("FLICKR_KEY", "XXXXXXXXXXXXXXXXXXXX");  //GET YOUR OWN API KEY
 
 $payload = json_decode(file_get_contents('php://input'), true);
 if(!$payload) {
-	//fake data if none supplied
-	$payload = json_decode("{\"1\": 20724248,\"2\": -2508086,\"3\": \"c\"}", true);
+	$payload = json_decode("{\"1\": 507242.48,\"2\": -35080.86,\"3\": \"c\"}", true);
 }	
-
 $lat = $payload[1] / 1000000;
 $long = $payload[2] / 1000000;
 $units = $payload[3];
 
-$success = false;
+$flickrResponse = get_data('http://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&format=json&api_key=' . FLICKR_KEY . '&lat=' . $lat . '&lon=' . $long);
+$flickrResponse = json_decode(substr($flickrResponse, 14, strlen($flickrResponse) - 15), true);
+$woeid = $flickrResponse['places']['place'][0]['woeid'];
+$woename = $flickrResponse['places']['place'][0]['woe_name'];
+if($woeid) {
+	$xml = simplexml_load_file('http://weather.yahooapis.com/forecastrss?w=' . $woeid . '&u=' . $units);
+	$xml->registerXPathNamespace('yweather', 'http://xml.weather.yahoo.com/ns/rss/1.0');
 
-if(!$success) {
-	$flickrResponse = get_data('http://api.flickr.com/services/rest/?method=flickr.places.findByLatLon&format=json&api_key=' . FLICKR_KEY . '&lat=' . $lat . '&lon=' . $long);
-	$flickrResponse = json_decode(substr($flickrResponse, 14, strlen($flickrResponse) - 15), true);
-	$woeid = $flickrResponse['places']['place'][0]['woeid'];
-	$woename = $flickrResponse['places']['place'][0]['woe_name'];
+	$condition = $xml->channel->item->xpath('yweather:condition');
+	$code = (int)$condition[0]['code'];
+	$temperature = (int)round($condition[0]['temp']);
+
+	//$astronomy = $xml->channel->xpath('yweather:astronomy');
+	//$sunrise = str_replace(' ', '', $astronomy[0]['sunrise']);
+	//$sunset = str_replace(' ', '', $astronomy[0]['sunset']);
+
+	$forecast = $xml->channel->item->xpath('yweather:forecast');
+	$forecasthigh = (int)$forecast[0]['high'];
+	$forecastlow = (int)$forecast[0]['low'];
+
+	//$location = $xml->channel->xpath('yweather:location');
+	//$city = $location[0]['city'];
 }
+else {
 
-$xml = simplexml_load_file('http://weather.yahooapis.com/forecastrss?w=' . $woeid . '&u=' . $units);
-$xml->registerXPathNamespace('yweather', 'http://xml.weather.yahoo.com/ns/rss/1.0');
-$condition = $xml->channel->item->xpath('yweather:condition');
-$temperature = round($condition[0]['temp']);
+	$code = 3200;
+	$temperature = 333;
+	$forecasthigh = 333;
+	$forecasthigh = 333;
 
-$astronomy = $xml->channel->xpath('yweather:astronomy');
-$sunrise = str_replace(' ', '', $astronomy[0]['sunrise']);
-$sunset = str_replace(' ', '', $astronomy[0]['sunset']);
-
-$forecast = $xml->channel->item->xpath('yweather:forecast');
-$forecasthigh = $forecast[0]['high'];
-$forecastlow = $forecast[0]['low'];
-
-$location = $xml->channel->xpath('yweather:location');
-$city = $location[0]['city'];
+}
 
 // yahoo code => watch face icon id // yahoo condition => watch face condition
 $icons = array(
@@ -63,7 +68,7 @@ $icons = array(
 	22 => 6, //smoky => fog
 	23 => 5, //blustery => wind
 	24 => 5, //windy => wind
-	25 => 14, //cold => cold
+	25 => 13, //cold => cold
 	26 => 7, //cloudy => cloudy
 	27 => 9, //mostly cloudy (night) => partly-cloudy-night
 	28 => 8, //mostly cloudy (day) => partly-cloudy-day
@@ -74,7 +79,7 @@ $icons = array(
 	33 => 9, //fair (night) => partly-cloudy-night
 	34 => 8, //fair (day) => partly-cloudy-day
 	35 => 12, //mixed rain and hail => rain-sleet
-	36 => 15, //hot => hot
+	36 => 14, //hot => hot
 	37 => 10, //isolated thunderstorms => thunder
 	38 => 10, //scattered thunderstorms => thunder
 	39 => 10, //scattered thunderstorms => thunder
@@ -86,31 +91,34 @@ $icons = array(
 	45 => 10, //thundershowers => thunder
 	46 => 3, //snow showers => snow
 	47 => 10, //isolated thundershowers => thunder
-	3200 => 16 //not available
+	3200 => 15 //not available
 );
 
 $data = array();
-//careful not to send too much data, can't exceed 100 bytes
-$data[1] = array('b', $icons[(int) $condition[0]['code']]);
-$data[2] = (int)$temperature; //array('s', round($condition[0]['temp'])); 
+
+$data[1] = $icons[$code];
+$data[2] = $temperature;
 //$data[3] = (string)$sunrise;
 //$data[4] = (string)$sunset;
-$data[5] = (int)$forecasthigh; //(string)$forecasthigh;
-$data[6] = (int)$forecastlow; //(string)$forecastlow;
-//$data[7] = (string)$city;
-
+$data[5] = $forecasthigh; 
+$data[6] = $forecastlow;
+//$ata[7] = (string)$city;
+//$data[8] = (int)$temperature;
 
 /*
 $logentry = date('Y-m-d H:i:s') . PHP_EOL . json_encode($payload) . PHP_EOL . json_encode($data) . PHP_EOL; 
-
 $fp = @fopen('log.txt', 'a');  
 fputs($fp, $logentry . PHP_EOL);  
 @fclose($fp);  
 */
 
 header('Content-Type: application/json');
-header('Cache-Control: max-age=840'); //840 seconds = 14 minutes
-echo(json_encode($data));
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT"); 
+header("Cache-Control: no-store, no-cache, must-revalidate"); 
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+print json_encode($data);
 
 
 function get_data($url) {
